f6fe0d56bdb049caaff6764af93db1fc62b40d62
Maksim Shlomin <maksim.shlomin@iqoption.com>
2018-03-06 22:08:28 +0300

    who watcher got smart logic

--- watchers/who_watcher.go
+++ watchers/who_watcher.go
@@ -3,15 +3,23 @@ package watchers
 import (
 	"time"
 	"log"
+	"github.com/sepuka/gowatcher/parsers"
 )
 
 const (
 	whoCommand = "who"
-	whoLoopInterval = time.Hour * 3
+	whoLoopInterval = time.Minute * 1
+	nosendCounterLimit = 59
+)
+
+var (
+	lastUsers = 0
+	nosendCounter = 0
 )
 
 func Who(config Configuration) {
 	result := RunCommand(whoCommand)
+	lastUsers = parsers.Cnt(result.GetText())
 	SendMessage(result, config)
 
 	for {
@@ -22,7 +30,20 @@ func Who(config Configuration) {
 				log.Printf("Watcher %v failed: %v", result.GetName(), result.GetError())
 				break
 			}
+
+			sendMessage(result, config)
+		}
+	}
+}
+
+func sendMessage(result WatcherResult, config Configuration) {
+	if parsers.Cnt(result.GetText()) > lastUsers {
+		SendUrgentMessage(result, config)
+	} else {
+		if nosendCounter > nosendCounterLimit {
+			nosendCounter = 0
 			SendMessage(result, config)
 		}
 	}
+	nosendCounter++
 }
